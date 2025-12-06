75c1105587f4987789052624a76848ddc5ebf058
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-06 14:51:52 +0500

    admin authentication middleware is added successfully

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { addDoctor, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 // After importing our required things, we will create routes. For that, first we will create router 
 
 const adminRouter = express.Router();
 
-adminRouter.post("/add-doctor", upload.single('image'),addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single('image'),addDoctor);
 adminRouter.post("/login",loginAdmin);
  
 export default adminRouter;
