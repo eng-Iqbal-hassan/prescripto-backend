0d248e69f180505d9db96c0983e8b30f48ac21af
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-09 16:41:43 +0500

    change availabity controller is added

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,1 +1,19 @@
 // Here, we will write multiple functions for multiple APIs
+import doctorModel from "../models/doctorModel.js";
+
+
+// We have added the avaialabilty functionality in doctor controller because we need this functionality in both admin and doctor portal 
+
+const changeAvailability = async (req,res) => {
+    try {
+        const {docId} = req.body;
+        const docData = await doctorModel.findById(docId);
+        await doctorModel.findByIdAndUpdate(docId,{available: !docData.available})
+        res.json({success:true, message: "Availability changed"});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+export {changeAvailability};
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { addDoctor, allDoctors, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailability } from "../controllers/doctorController.js";
 
 // After importing our required things, we will create routes. For that, first we will create router 
 
@@ -10,5 +11,6 @@ const adminRouter = express.Router();
 adminRouter.post("/add-doctor", authAdmin, upload.single('image'),addDoctor);
 adminRouter.post("/login",loginAdmin);
 adminRouter.post("/all-doctors", authAdmin,allDoctors);
+adminRouter.post("/change-availability", authAdmin,changeAvailability);
  
 export default adminRouter;
