9c33782726c483416065f7543f76295509e06590
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-09 17:23:33 +0500

    api is made for all doctors to show in frontend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
 
 // app config;
 
@@ -21,6 +22,7 @@ app.use(cors());
 
 app.use('/api/admin',adminRouter)
 // FRom here, whenever we will hit the endpoint localhost:4000/api/admin/add-doctor. From here it will go the admin routes and will chase the function which last parm is add-doctor. This function is addDoctor in adminController. In short on hit of this mentioned url, addDoctor function will be executed.
+app.use('/api/doctor',doctorRouter)
 
 app.get("/", (req,res) => {
     res.send("API is working greatly")
